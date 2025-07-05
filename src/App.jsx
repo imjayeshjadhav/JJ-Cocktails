f6bc28b88dabe6684d1a9943a1a0fda7c1c8b31c
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-07-05 20:16:33 +0530

    Setup and landing page UI

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import gsap from 'gsap'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
+    </main>
+  )
+}
+
+export default App
